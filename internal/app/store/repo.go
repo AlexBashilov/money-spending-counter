8ba93c83dead36c5150a7f384d5ff7781e6b1457
aleksejj.bashilov <aleksejj.bashilov@vitech.team>
2024-06-03 23:23:51 +0300

    исправл ручку GetAllItems

--- internal/app/store/repo.go
+++ internal/app/store/repo.go
@@ -4,7 +4,7 @@ import "booker/internal/app/model"
 
 type BookerRepository interface {
 	CreateItems(items *model.UserCostItems) error
-	GetAllItems() (map[string]interface{}, error)
+	GetAllItems() ([]map[string]interface{}, error)
 	GetOnlyOneItem(id int) (*model.UserCostItems, error)
 	DeleteItems(id int) error
 }
