9bda6b39b5417ed44668bffaaebe30aeefadb949
aleksejj.bashilov <aleksejj.bashilov@vitech.team>
2024-06-03 17:13:37 +0300

    Добавил ручку update items

---
+++ internal/app/apiserver/helper.go
@@ -0,0 +1,25 @@
+package apiserver
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
+	}
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
