1be250b8534e68118a6bde1f4cd6b193311064b4
aleksejj.bashilov <aleksejj.bashilov@vitech.team>
2024-06-03 21:35:47 +0300

    довавил ручку handleGetOnlyOneItem

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -1,13 +1,10 @@
 package apiserver
 
 import (
-	"booker/internal/app/model"
 	"booker/internal/app/store"
-	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"net/http"
-	"strconv"
 )
 
 type server struct {
@@ -35,80 +32,7 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (s *server) configureRouter() {
 	s.router.HandleFunc("/create_items", s.handleItemsCreate()).Methods("POST")
 	s.router.HandleFunc("/get_all_items", s.handleGetItems).Methods("GET")
+	s.router.HandleFunc("/get_only_one_items/{id:[0-9]+}/", s.handleGetOnlyOneItem).Methods("GET")
 	s.router.HandleFunc("/delete_items/{id:[0-9]+}/", s.handleDeleteItems).Methods("DELETE")
 	s.router.HandleFunc("/update_items/{id:[0-9]+}/", s.handleItemsUpdate()).Methods("POST")
-}
-
-func (s *server) handleItemsCreate() http.HandlerFunc {
-	type request struct {
-		ItemName    string `json:"item_name"`
-		Code        int    `json:"code"`
-		Description string `json:"description"`
-	}
-	return func(w http.ResponseWriter, r *http.Request) {
-		req := &request{}
-		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-			s.error(w, r, http.StatusBadRequest, err)
-			return
-		}
-		u := &model.UserCostItems{
-			ItemName:    req.ItemName,
-			Code:        req.Code,
-			Description: req.Description,
-		}
-		if err := s.store.Booker().CreateItems(u); err != nil {
-			s.error(w, r, http.StatusUnprocessableEntity, err)
-			return
-		}
-		s.respond(w, r, http.StatusCreated, u)
-	}
-}
-
-func (s *server) handleGetItems(w http.ResponseWriter, r *http.Request) {
-	res, err := s.store.Booker().GetAllItems()
-	if err != nil {
-		s.error(w, r, http.StatusUnprocessableEntity, err)
-	}
-	respondWithJSON(w, http.StatusOK, res)
-}
-
-func (s *server) handleDeleteItems(w http.ResponseWriter, r *http.Request) {
-	eventID, _ := strconv.Atoi(mux.Vars(r)["id"])
-
-	if err := s.store.Booker().DeleteItems(eventID); err != nil {
-		s.error(w, r, http.StatusUnprocessableEntity, err)
-	}
-
-	respondWithJSON(w, http.StatusOK, http.Response{})
-}
-
-func (s *server) handleItemsUpdate() http.HandlerFunc {
-	type request struct {
-		ItemName    string `json:"item_name"`
-		Code        int    `json:"code"`
-		Description string `json:"description"`
-	}
-	return func(w http.ResponseWriter, r *http.Request) {
-		eventID, _ := strconv.Atoi(mux.Vars(r)["id"])
-
-		if err := s.store.Booker().DeleteItems(eventID); err != nil {
-			s.error(w, r, http.StatusUnprocessableEntity, err)
-		}
-		req := &request{}
-		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-			s.error(w, r, http.StatusBadRequest, err)
-			return
-		}
-		u := &model.UserCostItems{
-			ItemName:    req.ItemName,
-			Code:        req.Code,
-			Description: req.Description,
-		}
-
-		if err := s.store.Booker().CreateItems(u); err != nil {
-			s.error(w, r, http.StatusUnprocessableEntity, err)
-			return
-		}
-		s.respond(w, r, http.StatusCreated, u)
-	}
 }
